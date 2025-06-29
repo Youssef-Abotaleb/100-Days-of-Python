114d70ca5a49aeecda4b1841ec1c5407640f552e
Youssef Abotaleb <eng.youssef.abotaleb@gmail.com>
2025-06-29 13:16:31 +0300

    fr

--- Day-7/hangman.py
+++ Day-7/hangman.py
@@ -11,17 +11,36 @@ for letter in chosen_word :
     place_holder += "_"
 print (f"{place_holder}\n")
 
+game_over = False
 
+correct_letters =[]
 
-while lives > 0 :
+while not game_over :
     guess = input("guess a letter : ").lower()
+
     display = ""
+
     for letter in chosen_word :
         if letter == guess :
             display += letter
+            correct_letters.append(guess)
+        elif letter in correct_letters:
+            display+=letter
         else:
              display += "_"
-             lives = lives - 1
     
     print (display)
 
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0 :
+            game_over = True
+            print("You Lose.")
+        
+    
+   
+    if "_" not in display:
+        game_over = True
+        print("You Win.")
+    
+    
